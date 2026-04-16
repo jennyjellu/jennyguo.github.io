589940ff070a97bf8afc66d6150e24ff98a3b620
jennyjellu <jennyguo507@gmail.com>
2026-04-16 19:20:48 -0400

    Add bucktoothBunny.js with bunny drawing

---
+++ bucktoothBunny.js
@@ -0,0 +1,16 @@
+ellipse(150, 70, 60, 120);  // left ear
+ellipse(240, 70, 60, 120);  // right ear
+
+ellipse(200, 170, 150, 150);    // face
+
+var eyesize = 9;
+fill(0, 0, 0);
+ellipse(170, 150, eyesize, eyesize);  // left eye
+ellipse(230, 150, eyesize, eyesize);  // right eye
+
+line(150, 200, 250, 200);   // mouth
+
+noFill();
+var teethsize = 64;
+rect(185, 200, 15, teethsize); // left tooth
+rect(200, 200, 15, teethsize); // right tooth
